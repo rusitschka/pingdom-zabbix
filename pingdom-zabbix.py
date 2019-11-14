2619629b3404bb84caaa486d189cb4247a318f58
Steffen Rusitschka <rusi@gmx.de>
2019-11-14 13:14:16 +0100

    support statuscake and python3

--- pingdom-zabbix.py
+++ pingdom-zabbix.py
@@ -7,12 +7,12 @@ import json
 import re
 import requests
 import subprocess
-import ConfigParser
+from configparser import ConfigParser
 
 #
 # pull in the config
 #
-config = ConfigParser.ConfigParser()
+config = ConfigParser()
 config.read('pingdom-zabbix.ini')
 
 #
@@ -20,13 +20,13 @@ config.read('pingdom-zabbix.ini')
 #
 def zabbix_trapper(cmd_args):
     try:
-        print ' '.join(cmd_args)
-        print subprocess.check_output(cmd_args)
+        print(' '.join(cmd_args))
+        print(subprocess.check_output(cmd_args))
     except subprocess.CalledProcessError as e:
-        print "EXCEPTION"
-        print "returncode:", e.returncode
-        print "cmd:", e.cmd
-        print "output:", e.output
+        print("EXCEPTION")
+        print("returncode:", e.returncode)
+        print("cmd:", e.cmd)
+        print("output:", e.output)
 
 #
 # pull pingdom data (checks and values)
@@ -42,11 +42,26 @@ def pingdom_data(pingdom_response):
     return data
 
 #
+# pull statuscake data (checks and values)
+#
+def statuscake_data(statuscake_response):
+    data = []
+    for check in statuscake_response.json():
+        status = check['Status'].lower()
+        if check['Paused'] == True:
+            status = "paused"
+        data.append({
+            'name': check['WebsiteName'],
+            'status': status
+        })
+    return data
+
+#
 # pingdom checks -> zabbix discovery
 #
-def zabbix_discovery(pingdom_data):
+def zabbix_discovery(data):
     discovery = []
-    for check in pingdom_data:
+    for check in data:
         discovery.append(
             {"{#NAME}": str(check['name'])}
         )
@@ -56,15 +71,15 @@ def zabbix_discovery(pingdom_data):
         '-p', config.get('ZABBIX', 'port'),
         '-s', config.get('ZABBIX', 'host'),
         '-k', config.get('ZABBIX', 'key1'),
-        '-o', "'{ \"data\": " + json.dumps(discovery) + " }'"
+        '-o', json.dumps({ 'data': discovery })
     ]
     zabbix_trapper(cmd_args)
 
 #
 # pingdom status -> zabbix trapper
 #
-def zabbix_status(pingdom_data):
-    for check in pingdom_data:
+def zabbix_status(data):
+    for check in data:
         # turn 'up' into 1 and 'down' into 0
         status = 0
         if check['status'] == 'up':
@@ -75,14 +90,14 @@ def zabbix_status(pingdom_data):
             '-p', config.get('ZABBIX', 'port'),
             '-s', config.get('ZABBIX', 'host'),
             '-k', config.get('ZABBIX', 'key2') + '[' + str(check['name']) + ']',
-            '-o', str(status)
+            '-o', json.dumps({ 'data': status })
         ]
         zabbix_trapper(cmd_args)
 
 #
 # pingdom lastresponsetime -> zabbix trapper
 #
-def zabbix_resptime(pingdom_data):
+def zabbix_resptime(data):
     for check in data:
         cmd_args = [
             'zabbix_sender',
@@ -90,11 +105,11 @@ def zabbix_resptime(pingdom_data):
             '-p', config.get('ZABBIX', 'port'),
             '-s', config.get('ZABBIX', 'host'),
             '-k', config.get('ZABBIX', 'key3') + '[' + str(check['name']) + ']',
-            '-o', str(check['resptime'])
+            '-o', json.dumps({ 'data': str(check['resptime']) })
         ]
         zabbix_trapper(cmd_args)
 
-try:
+def fetch_pingdom():
     # pingdom variables (convenience)
     pingdom = dict(
         apiurl   = config.get('PINGDOM', 'apiurl'),
@@ -113,7 +128,35 @@ try:
         zabbix_status(data)
         zabbix_resptime(data)
     else:
-        print "EXCEPTION: Bad Request; HTTP {}".format(str(res.status_code))
+        print("EXCEPTION: Bad Request; HTTP {}".format(str(res.status_code)))
+
+def fetch_statuscake():
+    # statuscake variables (convenience)
+    statuscake = dict(
+        apiurl   = config.get('STATUSCAKE', 'apiurl'),
+        apikey   = config.get('STATUSCAKE', 'apikey'),
+        username = config.get('STATUSCAKE', 'username'),
+    )
+    # connect to statuscake
+    res = requests.get(statuscake['apiurl'], headers={'API': statuscake['apikey'], 'Username': statuscake['username']})
+    if res.status_code == requests.codes.ok:
+        # fetch statuscake data (checks and values)
+        data = statuscake_data(res)
+        # statuscake checks -> zabbix discovery
+        zabbix_discovery(data)
+        # statuscake status -> zabbix values
+        zabbix_status(data)
+    else:
+        print("EXCEPTION: Bad Request; HTTP {}".format(str(res.status_code)))
+
+
+try:
+    if 'PINGDOM' in config:
+        fetch_pingdom()
+    elif 'STATUSCAKE' in config:
+        fetch_statuscake()
+    else:
+        print("EXCEPTION: Neither PINGDOM nor STATUSCAKE section found in config!")
 
 except Exception as e:
-    print "EXCEPTION: {}".format(str(e))
+    print("EXCEPTION: {} in line {}".format(str(e)))
